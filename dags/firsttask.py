52ce26059df6b2f31a283aeb907a30c45f5712c0
Ramesh <ramesh.senthurpandi@mollie.com>
2023-12-28 20:06:34 +0100

    **Added tasks with comments**

--- dags/firsttask.py
+++ dags/firsttask.py
@@ -1,46 +1,63 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-# from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
 
-def print_message(task_name):
-    print(f"I'm in {task_name}")
+# Define a simple Python function for Task A
+def task_a_function():
+    print("Executing Task A")
 
+# Define a simple Python function for Task B
+def task_b_function():
+    print("Executing Task B")
+
+# Define a simple Python function for Task C
+def task_c_function():
+    print("Executing Task C")
+
+# Define default arguments for the DAG
 default_args = {
-    'owner': 'Ramesh S',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'owner': 'Ramesh',                # Owner of the DAG
+    'start_date': datetime(2023, 1, 1), # Start date for the DAG
+    'retries': 1,                       # Number of retries for each task
+    'retry_delay': timedelta(minutes=1),# Delay between retries
 }
 
+# Create a DAG instance
 dag = DAG(
-    'My-Downstream-Task',
-    default_args=default_args,
-    description='A DAG to print messages for each task',
-    schedule_interval=timedelta(days=1),
+    'first_example_DAG',                      # Unique identifier for the DAG
+    default_args=default_args,          # Default arguments
+    description='A simple example DAG', # Description of the DAG
+    schedule_interval=timedelta(days=1),# Schedule interval for DAG runs (daily in this case)
 )
 
+# Create Task A with a PythonOperator
 task_a = PythonOperator(
-    task_id='task_a',
-    python_callable=print_message,
-    op_kwargs={'task_name': 'Task A'},
-    dag=dag,
+    task_id='task_a',                   # Unique identifier for the task
+    python_callable=task_a_function,    # Python function to be executed
+    dag=dag,                            # Associated DAG
 )
 
+# Create Task B with a PythonOperator
 task_b = PythonOperator(
-    task_id='task_b',
-    python_callable=print_message,
-    op_kwargs={'task_name': 'Task B'},
-    dag=dag,
+    task_id='task_b',                   # Unique identifier for the task
+    python_callable=task_b_function,    # Python function to be executed
+    dag=dag,                            # Associated DAG
 )
 
+# Create Task C with a PythonOperator
 task_c = PythonOperator(
-    task_id='task_c',
-    python_callable=print_message,
-    op_kwargs={'task_name': 'Task C'},
-    dag=dag,
+    task_id='task_c',                   # Unique identifier for the task
+    python_callable=task_c_function,    # Python function to be executed
+    dag=dag,                            # Associated DAG
 )
 
-task_a >> task_b
-task_a >> task_c
+# Define the execution sequence (Task A -> Task B -> Task C)
+task_a >> task_b >> task_c
+
+
+# Here's a step-by-step explanation:
+# * Task A will be executed first.
+# * Once Task A completes successfully, Task B will be triggered.
+# * Task B will wait for the successful completion of Task A before starting.
+# * After Task B completes successfully, Task C will be triggered.
+# * Task C will wait for the successful completion of Task B before starting.
